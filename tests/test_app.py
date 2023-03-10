e05fc8d40c62764e67f75f075d671a673005b0de
Shahin Azad <hi@shahinism.com>
2023-03-10 19:13:04 +0100

    feat (*): Add `create_repo` and `create_repo_name` methods.

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from app import create_repo_name
+
+
+@pytest.mark.parametrize("company,project,expected", [
+    ["company", "project", "company_project"],
+    ["Company", "Project", "company_project"],
+    ["Green Company", "My Project", "green-company_my-project"],
+])
+def test_create_repo_name(company, project, expected):
+    assert create_repo_name(company, project) == expected
+    
