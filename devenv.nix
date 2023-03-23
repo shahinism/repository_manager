d17906737cf44c76d41a477a02172301d3e28bae
Shahin Azad <hi@shahinism.com>
2023-03-23 14:04:25 +0100

    initialize devenv.

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+let python-packages = p: with p; [ pip python-lsp-server ];
+in {
+  # https://devenv.sh/basics/
+  env.GREET = "🛠️ Let's hack 🧑🏻‍💻";
+
+  # # https://devenv.sh/packages/
+  packages = with pkgs; [ (python3.withPackages python-packages) ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo $GREET";
+
+  enterShell = ''
+    hello
+  '';
+
+  # https://devenv.sh/languages/
+  languages.python.enable = true;
+
+  # Make diffs fantastic
+  difftastic.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks = {
+    black.enable = true;
+    nixfmt.enable = true;
+  };
+}
